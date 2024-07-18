27e4a49e41f52c2d07fe1865eaaf9f1e00293aa0
leonidig <leonid200955@gmail.com>
2024-07-19 00:52:11 +0300

    сan writes some code and keyboards, need udst sqlite & some features like  request phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telethon import TelegramClient, events
 from telethon.tl.custom import Button as button
+from telethon.events import CallbackQuery
 from os import getenv
 
 
@@ -16,26 +17,48 @@ async def start(event):
     sender = await event.get_sender()
     first_name = sender.first_name
     buttons=[
-                button.inline("США", b"usa"),
-                button.inline("УКРАИНА", b"ukr"),
-                button.inline("НИДЕРЛАНДЫ", b"ndr"),
-                ]
-    await event.respond(f'Hello, {first_name} ill help you with finding the capital of city', buttons=buttons)
+                button.inline("Ukraine", b"ukraine"),
+            ]
 
-@client.on(events.CallbackQuery(pattern=b"usa"))
-async def nikoglai_answer(event):
-    await event.respond("341 mln")
-
-
-@client.on(events.CallbackQuery(pattern=b"ukr"))
-async def nikoglai_answer(event):
-    await event.respond("31 mln")
+    await event.reply(f'Hello, {first_name} ill help you with finding the capital of city', buttons=buttons)
+    reply_buttons = [
+            # button.request_phone
+            button.text("Support", resize=True)
+        ]
+    await event.reply("Or you can wrote to our supporter", buttons=reply_buttons)
 
 
-@client.on(events.CallbackQuery(pattern=b"ndr"))
-async def nikoglai_answer(event):
-    await event.respond("17 mln")
 
+
+@client.on(events.CallbackQuery)
+async def enter_city_data(event):
+    if event.data == b'ukraine':
+        inline_buttons = [
+            [button.inline("Odessa", b"odessa")],
+            [button.inline("Kharkov", b"kharkov")],
+            [button.inline("Lvov", b"lvov")],
+            [button.inline("Kyiv", b"kyiv")],
+            [button.inline("Dnepropetrovsk", b"dnepropetrovsk")]
+        ]
+
+        await event.respond('Okey, please choose city', buttons=inline_buttons)
+    if event.data == b"odessa":
+        await event.respond("• Area: 236.9 km²\n• Population: 1 mln (2023)\n• Ukraine's largest port, an important centre of trade and transport\n• There are 2,500 kilometres of catacombs under the city.They were used by partisans in WWII.")
+    if event.data == b"kharkov":
+        await event.respond("• Area: 350 km²\n• Population: 1.4 mln (2023)\n• Major industrial, cultural, and educational centre\n• Home to Ukraine's oldest university, founded in 1804\n• Renowned for its Freedom Square, one of the largest city squares in Europe\n• Significant transport hub with an extensive metro system\n• Rich architectural heritage, including constructivist buildings")
+    if event.data == b"lvov":
+        await event.respond("• Area: 182 km²\n• Population: 720,000 (2023)\n• Important cultural, historical, and economic centre\n• Known for its well-preserved architecture and historic buildings\n• UNESCO World Heritage site due to its old town\n• Home to many universities, theatres, and museums\n• Famous for its coffee culture and annual festivals")
+    if event.data == b"kyiv":
+        await event.respond("• Area: 839 km²\n• Population: 2.9 mln (2023)\n• Capital and largest city of Ukraine\n• Major political, cultural, and economic centre\n• Home to numerous historical landmarks, including Kyiv Pechersk Lavra\n• Known for its vibrant arts scene and nightlife\n• Important educational hub with many universities")
+    if event.data == b"dnepropetrovsk":
+        await event.respond("• Area: 405 km²\n• Population: 1 mln (2023)\n• Major industrial and business centre\n• Important transportation hub with a significant river port\n• Known for its aerospace and manufacturing industries\n• Home to several universities and research institutions\n• Renowned for its long embankment along the Dnieper River")
+    else:
+        await event.answer("Erorr, please try again")
+
+
+@client.on(events.NewMessage(pattern="Support"))
+async def support(event):
+    await event.respond("You can write our suppoerter @big_pencil19")
 
 
 async def main():
